338bc69798b5cbfa4971d0a1d8095c3db114d594
lautarotranquilli <lautitranquilli9@outlook.es>
2021-05-07 20:05:36 -0300

    TrabajoPractico

---
+++ TP -4/Codigo/TP -4/Persona.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP__4
+{
+    class Persona
+    {
+        private String nombreApellido;
+        private Int32 dni;
+        private String domicilio;
+        private long telefono;
+        private String email;
+        private ActividadAutorizada actividad;
+
+        public string NombreApellido { get => nombreApellido; set => nombreApellido = value; }
+
+        public string Domicilio { get => domicilio; set => domicilio = value; }
+        public long Telefono { get => telefono; set => telefono = value; }
+        public string Email { get => email; set => email = value; }
+        internal ActividadAutorizada Actividad { get => actividad; set => actividad = value; }
+        public Int32 Dni { get => dni; set => dni = value; }
+
+        public Persona(ActividadAutorizada actividad)
+        {
+            this.actividad = actividad;
+        }
+
+        public void RegistrarPersona()
+        {
+            string linea;
+
+            Console.Write("\nIngrese nombre y apellido de la persona: ");
+            nombreApellido = Console.ReadLine();
+
+            Console.Write("Ingrese DNI de la persona: ");
+            linea = Console.ReadLine();
+            dni = Int32.Parse(linea);
+
+            Console.Write("Ingrese el domicilio de la persona: ");
+            domicilio = Console.ReadLine();
+
+            Console.Write("Ingrese el telefono de la persona: ");
+            linea = Console.ReadLine();
+            telefono = long.Parse(linea);
+
+            Console.Write("Ingrese el email de la persona: ");
+            email = Console.ReadLine();
+
+            Console.WriteLine("Ingrese la actividad que realiza la persona\n");
+            actividad.ListaActividades();
+
+            Console.WriteLine("\n");
+        }
+
+
+        public void ToStringPersona()
+        {
+            Console.WriteLine("\nNombre y apellido: " + nombreApellido);
+            Console.WriteLine("DNI: " + dni);
+            Console.WriteLine("Domicilio: " + domicilio);
+            Console.WriteLine("Telefono: " + telefono);
+            Console.WriteLine("Email: " + email);
+            Console.WriteLine("Actividad: " + actividad.Nombre);
+
+            if (Actividad.Autorizacion == true)
+            {
+                Console.WriteLine("Persona autorizada para circular.");
+
+            }
+
+            else
+            {
+                Console.WriteLine("Persona no autorizada para circular.");
+            }
+        }
+    }
+}
---
+++ TP -4/Codigo/TP -4/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TP__4
+{
+    class Program
+    {
+        static RegPersonas personasRegistradas = new RegPersonas();
+
+        static void Main(string[] args)
+        {
+            int op;
+            string linea;
+
+            do
+            {
+                Console.WriteLine("Ingrese la operacion a realizar: ");
+                Console.WriteLine("1. Registrar persona.");
+                Console.WriteLine("2. Autorizar ingreso.");
+                Console.WriteLine("3. Finalizar.");
+                Console.WriteLine("");
+                linea = Console.ReadLine();
+                op = int.Parse(linea);
+
+                switch (op)
+                {
+                    case 1:
+                        RegistrarPersona();
+                        break;
+
+                    case 2:
+                        AutorizarIngreso();
+                        break;
+
+                    case 3:
+                        //personasRegistradas.MuestreoPersonas(); Corroborar que se ingresaron correctamente las personas.
+                        Console.WriteLine("\nPrograma finalizado - Muchas gracias!");
+                        break;
+
+                    default:
+                        Console.WriteLine("\nValor incorrecto. Ingrese nuevamente\n");
+                        break;
+
+                }
+            } while (op != 3);
+
+
+
+        }
+
+        public static void RegistrarPersona()
+        {
+            int n;
+
+            Console.WriteLine("\tREGISTRO DE PERSONAS\n");
+
+            Console.Write("Cuantas personas desea ingresar: ");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var actividad = new ActividadAutorizada("Deporte");
+                _ = new ActividadAutorizada("Religion");
+                _ = new ActividadAutorizada("Educacion");
+                _ = new ActividadAutorizada("Politica");
+
+                var persona = new Persona(actividad);
+
+                Console.Write("\n\tPERSONA {0}", i+1);
+                persona.RegistrarPersona();
+
+                if (persona.Actividad.Autorizacion == true)
+                {
+                    Console.WriteLine("PERSONA AUTORIZADA PARA CIRCULAR.\n");
+                    personasRegistradas.AgregarPersona(persona);
+
+                }
+
+                else
+                {
+                    Console.WriteLine("PERSONA NO AUTORIZADA PARA CIRCULAR.\n");
+                    personasRegistradas.AgregarPersona(persona);
+                }
+            }
+                
+        }
+
+        public static void AutorizarIngreso()
+        {
+            Console.Write("\tVERIFICACION DE PERSONA");
+
+            personasRegistradas.BuscarDni();
+        }
+    }
+}
